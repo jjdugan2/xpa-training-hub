b3e60fc5d16f01a85dff532c7f895f66ab23fa42
Jorgen Jensen <liquid_llama@hotmail.com>
2024-04-02 18:14:05 -0600

    Added support to extract bolded phrases from chat

--- src/components/ResourceChatThread.tsx
+++ src/components/ResourceChatThread.tsx
@@ -13,10 +13,46 @@ interface Props {
 
 const ResourceChatThread = ({ index, thread, isFinalMessage }: Props) => {
   const [displayedMessage, setDisplayedMessage] = useState("");
-  const [currentCharIndex, setCurrentCharIndex] = useState(0); // Track the current character index
+  const [currentCharIndex, setCurrentCharIndex] = useState(0);
+  let boldTextArray: string[] = [];
 
   const isValidThread =
     index >= 2 && thread.message && thread.message.length > 0;
+
+  //--NOTE: this is in development.
+  useEffect(() => {
+    if (isValidThread && thread.direction === "incoming") {
+      const regex = /\*\*(.*?)\*\*/g;
+      let match;
+      while ((match = regex.exec(thread.message)) !== null) {
+        boldTextArray.push(match[1]);
+      }
+
+      let updatedMessage = thread.message;
+
+      if (boldTextArray.length > 0) {
+        //console.log("Array: ", boldTextArray);
+
+        boldTextArray.forEach((boldText, index) => {
+          const boldTextIndex = updatedMessage.indexOf(boldText);
+          if (boldTextIndex !== -1) {
+            //console.log("Found: ", boldText);
+            const boldTextLength = boldText.length;
+            const boldTextWithTags = `<b>${boldText}</b>`;
+            // Insert bold tags into the message string at the appropriate positions
+            updatedMessage =
+              updatedMessage.slice(0, boldTextIndex) +
+              boldTextWithTags +
+              updatedMessage.slice(boldTextIndex + boldTextLength);
+            // Remove the element from the boldTextArray
+            boldTextArray = boldTextArray.filter((text, idx) => idx !== index);
+          }
+        });
+      }
+
+      //console.log(updatedMessage);
+    }
+  }, [thread.message, isValidThread]);
 
   useEffect(() => {
     let timeoutId: string | number | NodeJS.Timeout | undefined;
@@ -25,11 +61,12 @@ const ResourceChatThread = ({ index, thread, isFinalMessage }: Props) => {
       timeoutId = setTimeout(() => {
         if (currentCharIndex < thread.message.length) {
           setDisplayedMessage((prevMessage) => {
+            let messageToDisplay = prevMessage;
             const nextChar = thread.message.charAt(currentCharIndex);
             if (nextChar === "\n") {
-              return prevMessage + "<br />"; // Insert a <br> element for new line
+              return messageToDisplay + "<br />";
             } else {
-              return prevMessage + nextChar;
+              return messageToDisplay + nextChar;
             }
           });
           setCurrentCharIndex((prevIndex) => prevIndex + 1);
